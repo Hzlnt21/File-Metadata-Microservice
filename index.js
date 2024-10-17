70f37f51df448bf668f297bb2f391bf33ff515cc
Hzlnt21 <ivannovicbachtiar2@gmail.com>
2024-10-17 14:17:33 +0700

    Rebrand

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const multer = require('multer');
+// Set upload ke folder "uploads"
+const upload = multer({ dest: 'uploads' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Route analisis file nya
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  // Mengambil data dari file yang diupload
+  let name = req.file.originalname;
+  let type = req.file.mimetype;
+  let size = req.file.size;
 
-
+  // Mengembalikan data
+  res.json({
+    name,
+    type,
+    size
+  });
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
